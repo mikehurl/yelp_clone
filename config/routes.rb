d23011e8c9ad8baef0019f1af4b9ef0452a05ccb
Simon <simon.conway@condaal.com>
2017-01-10 15:56:28 +0000

    Added user functionality using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :installs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 resources :restaurants do
   resources :reviews
 end
+
+root "restaurants#index"
+
 # get 'restaurants' => 'restaurants#index'
 # get 'restaurants/new' => 'restaurants#new'
 
