9d9c413f71cb5718d0eb94956e8b560e35c4b455
Simon <simon.conway@condaal.com>
2017-01-10 11:49:01 +0000

    challenge 9

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
 resources :restaurants
 
-get 'restaurants' => 'restaurants#index'
-
-get 'restaurants/new' => 'restaurants#new'
+# get 'restaurants' => 'restaurants#index'
+# get 'restaurants/new' => 'restaurants#new'
 
 end
