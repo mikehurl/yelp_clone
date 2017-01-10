108b673958835672e7a2e29ab4a6e58892372b89
Jack <jack_6894@hotmail.com>
2017-01-10 12:28:00 +0000

    about to start ex 10

--- yelp_clone/spec/features/restaurant_feature_spec.rb
+++ yelp_clone/spec/features/restaurant_feature_spec.rb
@@ -27,7 +27,18 @@ end
     click_link 'Add a restaurant'
     fill_in 'Name', with: 'KFC'
     click_button 'Create Restaurant'
-    expect(page).to have_content 'KFC'
+    expect(page).to have_content('KFC')
     expect(current_path).to eq '/restaurants'
+  end
+end
+
+context 'viewing restaurants' do
+  let!(:kfc){ Restaurant.create(name:'KFC') }
+
+  scenario 'lets a user view a restaurant' do
+    visit '/restaurants'
+    click_link ('KFC')
+    expect(page).to have_content('KFC')
+    expect(current_path).to eq("/restaurants/#{kfc.id}")
   end
 end
