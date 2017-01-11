2d49d596c8bbb9675970485f41a933f03837cda4
Jack <jack_6894@hotmail.com>
2017-01-11 15:16:35 +0000

    only signed in users can create, edit and add. guests can see the list of restaurants

--- yelp_clone/spec/features/restaurant_feature_spec.rb
+++ yelp_clone/spec/features/restaurant_feature_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
+require 'web_helpers'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
     scenario 'should display a prompt to add restaurants' do
       visit '/restaurants'
+      sign_up
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link 'Add a restaurant'
     end
@@ -24,6 +26,7 @@ end
   context 'creating restaurants' do
   scenario 'prompts user to fill out a form, then displays the new restaurant' do
     visit '/restaurants'
+    sign_up
     click_link 'Add a restaurant'
     fill_in 'Name', with: 'KFC'
     click_button 'Create Restaurant'
@@ -34,13 +37,14 @@ end
   context 'an invalid restaurant' do
     scenario 'does not let you submit a name that is too short' do
       visit('/restaurants')
+      sign_up
       click_link("Add a restaurant")
       fill_in("Name", with: "KF")
       click_button("Create Restaurant")
 
       expect(page).not_to have_css("h2", text: "KF")
       expect(page).to have_content("Error")
-    end 
+    end
   end
 end
 
@@ -53,7 +57,15 @@ context 'viewing restaurants' do
     expect(page).to have_content('KFC')
     expect(current_path).to eq("/restaurants/#{kfc.id}")
   end
+
+  scenario 'lets guest users only see list of restaurants' do
+    visit '/'
+    expect(page).not_to have_link("Add a restaurant")
+    expect(page).to have_content("KFC")
+  end
+
 end
+
 
 context 'editing restaurants' do
 
@@ -61,6 +73,7 @@ context 'editing restaurants' do
 
   scenario 'let a user edit a restaurant' do
     visit '/restaurants'
+    sign_up
     click_link('Edit KFC')
     fill_in('Name', with: 'Kentucky Fried Chicken')
     fill_in('Description', with: 'Deep Fried Goodness')
@@ -78,6 +91,7 @@ context 'deleting restaurants' do
 
   scenario 'removes a restaurant when a user clicks a delete link' do
     visit '/restaurants'
+    sign_up
     click_link('Delete KFC')
     expect(page).not_to have_content('KFC')
     expect(page).to have_content('Restaurant deleted successfully')
