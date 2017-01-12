d0ab1c1080971a3ad5609c95e828e037b4e712b8
Mike Hurl <mikehurl@gmail.com>
2017-01-12 15:04:06 +0000

    creates module to extend associations between models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,16 @@
 class Restaurant < ActiveRecord::Base
   validates :name, length: { minimum: 3 }, uniqueness: true
-  has_many :reviews, dependent: :destroy
+  belongs_to :user
+
+  has_many :reviews,
+    -> { extending WithUserAssociationExtension },
+    dependent: :destroy
+
 
   def build_review(attributes = {}, user)
     review = reviews.build(attributes)
     review.user = user
     review
   end
-  
+
 end
