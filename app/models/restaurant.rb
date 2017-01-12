3eb10f263144eb42e4e3d6b15d2c69b54250d228
Bryony Watson <bryonywatson1@googlemail.com>
2017-01-12 16:55:29 +0000

    final commit thursday

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,11 +4,10 @@ class Restaurant < ApplicationRecord
   belongs_to :user
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  has_many :reviews do
-    def build_with_user(attributes = {}, user)
-      attributes[:user] ||= user
-      build(attributes)
-    end
+  def build_review(attributes = {}, user)
+  review = reviews.build(attributes)
+  review.user = user
+  review
   end
 
 end
